2f24af428deadf3581765e3fe10d5dc7d87ad159
Webdev1 <webdev@netaesthetics.com>
2026-08-18 15:30:13 +0530

    add count digit

--- count_digit_in_integer/program.c
+++ count_digit_in_integer/program.c
@@ -2,5 +2,16 @@
 
 int main()
 {
-    
+    printf("Enter the number you want to count its digt\n");
+    int n;
+    scanf("%d", &n);
+    int count = 0;
+    int original = n;
+    while(n!=0)
+    {
+        int digit = n % 10;
+        count++;
+        n = n /10;
+    }
+    printf("The  number of digit in %d is %d\n" ,original, count);
 }
