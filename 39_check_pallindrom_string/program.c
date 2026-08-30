697ff9ce7ee8b8d8e5a0b31b1af7fd2e10c46cc9
Webdev1 <webdev@netaesthetics.com>
2026-08-30 23:27:04 +0530

    addded pallindrom check

--- 39_check_pallindrom_string/program.c
+++ 39_check_pallindrom_string/program.c
@@ -1,10 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+void reverse(char *p , int n)
+{
+    int start = 0;
+    int end = 0;
+    while(p[end] != '\0' && p[end] != '\n')
+    {
+        end++;
+    }
+    end = end -1;
+    while(start < end)
+    {
+        char temp = p[start];
+        p[start] = p[end];
+        p[end] = temp;
+        start++;
+        end--;
+    }
+
+   
+}
+
 int main()
 {
     char a[2000];
+    char b[2000];
     printf("Enter your string\n");
     fgets(a , 2000 , stdin);
-    printf("Your entered string is %s ", a);
+    int i = 0;
+    while(a[i] != '\0')
+    {
+        b[i] = a[i];
+        i++;
+    }
+    b[i] = '\0';
+    reverse(a , 3);
+    
+    int j = 0;
+    while(a[j] != '\0')
+    {
+        if(a[j] != b[j])
+        {
+            printf("The given string is not pallindrom\n");
+            return 0;
+        }
+        j++;
+    }
+
+    printf("The given string is pallindrom\n");
+  
 }
+
